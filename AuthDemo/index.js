2fc34c3fef94f4402ee8e4e19f8b5faf8dafda3c
AbdelRahman Alnajjar <>
2022-04-27 09:34:23 -0700

    Register/Sign_up
    Add to home page

---
+++ AuthDemo/index.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const app = express();
+const User = require("./models/user");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const { render } = require("ejs");
+
+//connecting to mongoose
+mongoose
+  .connect("mongodb://localhost:27017/authDemo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO ERROR!!!");
+    console.log(err);
+  });
+
+// so we can use the stylesheets
+app.use(express.static("AuthDemo"));
+app.use("/stylesheets", express.static(__dirname + "/stylesheets"));
+
+// for the ejs and to be able to render
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//to be able to parse req.body
+app.use(express.urlencoded({ extended: true }));
+// for the session
+app.use(session({ secret: "notagoodsecret" }));
+
+//middleware to help us verify if a user is logged in or not
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Sign up (Register) Routs (Start) -----------------------------
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+/*
+- extract username and password
+- hash the password
+- create new user
+- add to database
+  */
+app.post("/register", async (req, res) => {
+  const { firstname, lastname, username, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    firstname,
+    lastname,
+    username,
+    email,
+    password: hash,
+    balance: 0,
+  });
+
+  await user.save();
+  // after signing up, redirect to login page
+  res.redirect("/login");
+});
+// Sign up (Register) Routs (End) -----------------------------
+/*************************************************/
+// Login Routs (Start) -----------------------------
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+/*
+- extract the username and password
+- find the user with the username
+- after finding the user
+- compare the password to the hashed password
+- if a valid login, add to session
+*/
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (validPassword) {
+    // if a successful login, store user id in session
+    req.session.user_id = user._id;
+    res.render("profile", { user });
+    // res.redirect("/secret");
+  } else {
+    res.redirect("/login");
+  }
+});
+// Login Routs (End) -----------------------------
+/******************************************/
+// Log out Rout (Start) -----------------------------
+/*
+- to log out
+- remove user id from the session
+- redirect to home, or login page
+*/
+app.post("/logout", async (req, res) => {
+  //   req.session.user_id = null;
+  req.session.destroy(); //more powerful
+  res.redirect("/");
+});
+// log out Rout (End) -----------------------------
+
+// to protect (only valud if logged it)
+// add an if statment to check if session has user id
+app.get("/secret", requireLogin, (req, res) => {
+  res.render("secret");
+});
+
+app.listen(3000, () => {
+  console.log("SERRVING YOUR APP! on port 3000");
+});
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>);
+  </BrowserRouter>
+);
