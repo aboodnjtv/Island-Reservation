cdc7b1e57465b54092c687ce854d52b4dfb5720a
jtlystudent0 <jonathantly.01@gmail.com>
2022-05-01 18:19:40 -0700

    added sorting endpoints
    land_size, price, and rating so far

--- Backend/routes/island.js
+++ Backend/routes/island.js
@@ -13,95 +13,225 @@ const ObjectId = require("mongodb").ObjectId;
 
 const Island = require('../models/m_island.js');
 
-//if this doesn't work and the mongoose module can't be found, type npm install mongoose into terminal
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// // put after I've separated the island schema into it's own folder and file
-// // const Student = require('../models/island.js');
-
-// // create student schema & model
-// const islandSchema = new Schema({
-//     name: {
-//         type: String,
-//         required: [true, 'name field is required']
-//     },
-//     location: {
-//       type: String,
-//       required: [true, 'location field is required']
-//     },
-//     land_size: {
-//       type: Number,
-//       required: [true, 'land_size field is required']
-//     }, 
-//     amenities: {
-//       type: [String],
-//       // required: [true, 'name field is required']
-//     },
-//     isAvailable: {
-//       type: Boolean,
-//       // required: [true, 'name field is required']
-//     },
-//     pic_url: {
-//       type: String,
-//       // required: [true, 'name field is required']
-//     }
-// });
-
-// const Island = mongoose.model('island', islandSchema);
-
-// module.exports = Island;
-
-
 //sample call in postman: localhost:5000/api/islands
 islandRoutes.get('/islands', async (req, res) => {
-  try{
+  try
+  {
       // console.log("got inside");
       let db_client = dbo.getClient();
-      const data = await db_client.db("IR").collection("islands").find({}).toArray();
+      let data = await db_client.db("IR").collection("islands").find({}).toArray();
+
       res.send(data);
   }
-  catch(error){
+  catch(error)
+  {
       res.status(500).json({message: error.message});
   }
 })
 
+//sort by ascending land_size
+islandRoutes.get('/islands/land_size/asc', async (req, res) => {
+  try
+  {
+      let db_client = dbo.getClient();
+      let all_islands = await db_client.db("IR").collection("islands").find({}).toArray();
+        
+      let ls_map = new Map();
+      let ls_map_sorted = new Map();
+      let sorted_data = new Array();
 
+      for (let island = 0; island < all_islands.length; island++)
+      {
+        ls_map.set(all_islands[island], all_islands[island].land_size);
 
-// islandRoutes.get("/islands", async function(req,res,next){
-//   // console.log("trying to get");
-//   let db_connect = dbo.getDb();
-//   db_connect.collection("IR").find({}).toArray(
-//   // await Island.find({}).then(
-//   // Island.find({}).toArray(
-//     function(err, results) 
-//     {
-//       if (err) 
-//       {
-//         res.send({message: '500 Failure'});
-//         console.error(err); 
-//       }
-//       else 
-//       {
-//         console.log("successfully retrieved all of the islands");
-//         console.log(results);
-//         res.send({ message: '200 Success' , island_list: results}); 
-//       }
-//     }
-//   )
-// });
+      }
 
-// islandRoutes.get('/getAll', async (req, res) => {
-//   try{
-//       console.log("got inside");
-//       let db_client = dbo.getClient();
-//       const data = await db_client.db("IR").collection("islands").find({}).toArray();
-//       res.send(data);
-//   }
-//   catch(error){
-//       res.status(500).json({message: error.message});
-//   }
-// })
+      //sort the islands by ascending land size
+      //from code found on StackOverflow
+      ls_map_sorted = new Map([...ls_map.entries()].sort((size1, size2) => size1[1] - size2[1]));
+
+      for (let island of ls_map_sorted.keys())
+      {
+        sorted_data.push(island);
+      }
+
+      res.send(sorted_data);
+  }
+  catch(error)
+  {
+      res.status(500).json({message: error.message});
+  }
+})
+
+//sort by descending land_size
+islandRoutes.get('/islands/land_size/desc', async (req, res) => {
+  try
+  {
+      let db_client = dbo.getClient();
+      let all_islands = await db_client.db("IR").collection("islands").find({}).toArray();
+        
+      let ls_map = new Map();
+      let ls_map_sorted = new Map();
+      let sorted_data = new Array();
+
+      for (let island = 0; island < all_islands.length; island++)
+      {
+        ls_map.set(all_islands[island], all_islands[island].land_size);
+
+      }
+
+      //sort the islands by descending land size
+      //from code found on StackOverflow
+      ls_map_sorted = new Map([...ls_map.entries()].sort((size1, size2) => size2[1] - size1[1]));
+
+      for (let island of ls_map_sorted.keys())
+      {
+        sorted_data.push(island);
+      }
+
+      res.send(sorted_data);
+  }
+  catch(error)
+  {
+      res.status(500).json({message: error.message});
+  }
+})
+
+//sort by ascending price
+islandRoutes.get('/islands/price/asc', async (req, res) => {
+  try
+  {
+      let db_client = dbo.getClient();
+      let all_islands = await db_client.db("IR").collection("islands").find({}).toArray();
+        
+      let p_map = new Map();
+      let p_map_sorted = new Map();
+      let sorted_data = new Array();
+
+      for (let island = 0; island < all_islands.length; island++)
+      {
+        p_map.set(all_islands[island], all_islands[island].price);
+
+      }
+
+      //sort the islands by ascending price
+      //from code found on StackOverflow
+      p_map_sorted = new Map([...p_map.entries()].sort((price1, price2) => price1[1] - price2[1]));
+
+      for (let island of p_map_sorted.keys())
+      {
+        sorted_data.push(island);
+      }
+
+      res.send(sorted_data);
+  }
+  catch(error)
+  {
+      res.status(500).json({message: error.message});
+  }
+})
+
+//sort by descending price
+islandRoutes.get('/islands/price/desc', async (req, res) => {
+  try
+  {
+      let db_client = dbo.getClient();
+      let all_islands = await db_client.db("IR").collection("islands").find({}).toArray();
+        
+      let p_map = new Map();
+      let p_map_sorted = new Map();
+      let sorted_data = new Array();
+
+      for (let island = 0; island < all_islands.length; island++)
+      {
+        p_map.set(all_islands[island], all_islands[island].price);
+
+      }
+
+      //sort the islands by descending price
+      //from code found on StackOverflow
+      p_map_sorted = new Map([...p_map.entries()].sort((price1, price2) => price2[1] - price1[1]));
+
+      for (let island of p_map_sorted.keys())
+      {
+        sorted_data.push(island);
+      }
+
+      res.send(sorted_data);
+  }
+  catch(error)
+  {
+      res.status(500).json({message: error.message});
+  }
+})
+
+//sort by ascending rating
+islandRoutes.get('/islands/rating/asc', async (req, res) => {
+  try
+  {
+      let db_client = dbo.getClient();
+      let all_islands = await db_client.db("IR").collection("islands").find({}).toArray();
+        
+      let r_map = new Map();
+      let r_map_sorted = new Map();
+      let sorted_data = new Array();
+
+      for (let island = 0; island < all_islands.length; island++)
+      {
+        r_map.set(all_islands[island], all_islands[island].rating);
+
+      }
+
+      //sort the islands by ascending rating
+      //from code found on StackOverflow
+      r_map_sorted = new Map([...r_map.entries()].sort((rating1, rating2) => rating1[1] - rating2[1]));
+
+      for (let island of r_map_sorted.keys())
+      {
+        sorted_data.push(island);
+      }
+
+      res.send(sorted_data);
+  }
+  catch(error)
+  {
+      res.status(500).json({message: error.message});
+  }
+})
+
+//sort by descending rating
+islandRoutes.get('/islands/rating/desc', async (req, res) => {
+  try
+  {
+      let db_client = dbo.getClient();
+      let all_islands = await db_client.db("IR").collection("islands").find({}).toArray();
+        
+      let r_map = new Map();
+      let r_map_sorted = new Map();
+      let sorted_data = new Array();
+
+      for (let island = 0; island < all_islands.length; island++)
+      {
+        r_map.set(all_islands[island], all_islands[island].rating);
+
+      }
+
+      //sort the islands by descending rating
+      //from code found on StackOverflow
+      r_map_sorted = new Map([...r_map.entries()].sort((rating1, rating2) => rating2[1] - rating1[1]));
+
+      for (let island of r_map_sorted.keys())
+      {
+        sorted_data.push(island);
+      }
+
+      res.send(sorted_data);
+  }
+  catch(error)
+  {
+      res.status(500).json({message: error.message});
+  }
+})
 
 
 module.exports = islandRoutes;
