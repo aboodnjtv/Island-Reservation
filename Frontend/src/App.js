c9b80395fdc1b90698a9ddddb9ef2ab9cd37a312
mwen6 <103011669+mwen6@users.noreply.github.com>
2022-05-07 14:41:35 +0800

    update app

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AppIndex from './pages/AppIndex.js';
 import AppDetail from './pages/AppDetail.js';
 import Home from './pages/Home.js';
+import AppAbout from './pages/AppAbout.js';
 import { Routes, Route } from "react-router-dom";
 import SignUp from './pages/Signup';
 import Signin from './pages/Signin';
@@ -15,6 +16,7 @@ function App() {
         <Route path="/user/signup" element={<SignUp />} />
         <Route path="/list" element={<AppIndex />} />
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AppAbout />} />
       </Routes>
     </div>
   );
