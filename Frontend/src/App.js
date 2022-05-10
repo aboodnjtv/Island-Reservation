07a7edd14c0ae28b878fbacef1d4100897dcd879
Maiah Pardo <maiahpardo@Maiahs-MacBook-Pro.local>
2022-05-10 11:32:32 -0700

    added protected route for userhome page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import AppIndex from './pages/AppIndex.js';
 import AppDetail from './pages/AppDetail.js';
 import Home from './pages/Home.js';
-import { Routes, Route } from "react-router-dom";
 import SignUp from './pages/Signup';
 import SignIn from './pages/Signin';
+import UserHome from './pages/UserHome';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -15,6 +17,11 @@ function App() {
         <Route exact path="/user/signup" element={<SignUp />} />
         <Route exact path="/list" element={<AppIndex />} />
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/userhome" element={(
+          <ProtectedRoute isLoggedIn={localStorage.getItem("isAuthenticated")}>
+          <UserHome/>
+          </ProtectedRoute>
+        )} />
       </Routes>
     </div>
   );
