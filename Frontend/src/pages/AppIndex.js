b40bbf66695d57afc9984a1c04f4a958905563c0
mwen6 <103011669+mwen6@users.noreply.github.com>
2022-05-16 20:52:33 +0800

    update index page

--- Frontend/src/pages/AppIndex.js
+++ Frontend/src/pages/AppIndex.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import './appIndex.css';
 import CardView from '../components/CardView.js';
-import Navbar from '../components/Navbar.js'
+import HttpRequest from '../components/HttpRequest.js';
 /**
  * app home
  */
  class AppIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    }
+    //http请求
+    new HttpRequest().get("/islands")
+    .then((response) => {
+      this.setState({
+        list: response.data
+      });
+    })
+  }
   render(){
-    return (
-    <>
-    <Navbar page="Gallery"/>
-    <div className="row">
-      <div className="col-lg-3 col-md-4 col-sm-12">
-        <CardView title='Providence Cialis Island' distance='4.9' area='1278781' cover='1.jpg' />
-      </div>
-      <div className="col-lg-3 col-md-4 col-sm-12">
-        <CardView title='Maui, Hawaii' distance='8.9' area='45780' cover='2.jpg' />
-      </div>
-      <div className="col-lg-3 col-md-4 col-sm-12">
-        <CardView title='Santorini' distance='8.9' area='85124545' cover='3.jpg' />
-      </div>
-      <div className="col-lg-3 col-md-4 col-sm-12">
-        <CardView title='Madeira, Portugal' distance='40.9' area='878112' cover='4.jpg' />
-      </div>
+    const list = this.state.list;
+    console.log(list);
+    const listItems = list.map((item) =>
+    <div key={item._id} className="col-lg-3 col-md-4 col-sm-12">
+      <CardView title={item.name} distance={item.rating} area={item.land_size} cover={item.islandImg} />
     </div>
-    </>
-  );
+    );
+    return (
+    <div className="row">
+      {listItems}
+    </div>);
   }
  }
 
