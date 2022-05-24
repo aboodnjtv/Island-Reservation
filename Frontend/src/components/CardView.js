d130199403f47636229fdf00afdd794b70019b31
Wilson <wilsonchen.code@gmail.com>
2022-05-23 21:30:50 -0700

    Fixed adding a reservation

--- Frontend/src/components/CardView.js
+++ Frontend/src/components/CardView.js
@@ -2,21 +2,16 @@ import React from "react";
 
 class CardView extends React.Component {
   constructor(props) {
-    const is_available_ = "Not Available";
-
     super(props);
     console.log(props);
     this.state = {
       cover: props.cover || "",
       title: props.title || "",
-      distance: props.distance || "",
+      rating: props.rating || "",
       area: props.area || "",
-      is_available: props.is_available_ || "",
-      availability: is_available_,
+      price: props.price || "",
+      id: props.id,
     };
-    if (this.props.is_available) {
-      this.state.availability = "Available";
-    }
   }
   render() {
     return (
@@ -27,16 +22,13 @@ class CardView extends React.Component {
             <div className="card-body">
               <h5 className="card-title">{this.state.title}</h5>
               <div className="card-info">
-                <div className="card-info-number">{this.state.distance} km</div>
+                <div className="card-info-number">{this.state.rating}</div>
               </div>
               <div className="card-info-dot">Area {this.state.area} sq.m</div>
               <div className="card-info-dot">
-                is_available: {this.state.availability}
+                Price: ${this.state.price}/night
               </div>
-              <a href="/detail/1" className="btn btn-link">
-                Detail
-              </a>
-              <a href="/reserve" className="btn btn-primary btn-lg active -sm">
+              <a href={`/reserve/?island=${this.state.id}`} className="btn btn-primary btn-lg active -sm">
                 RESERVE NOW
               </a>
             </div>
