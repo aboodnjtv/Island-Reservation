f0e21798017c5bca63a5549b3cb3b4c82aed9b04
Maiah Pardo <maiahpardo@Maiahs-MacBook-Pro.local>
2022-05-25 15:04:32 -0700

    Fixed Reservations on user homepage to display formatted dates

--- Frontend/src/components/ReservationCard.js
+++ Frontend/src/components/ReservationCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from 'moment';
 
 class ReservationCard extends React.Component {
 
@@ -9,15 +10,14 @@ class ReservationCard extends React.Component {
 
   render() {
     return (
-      <div className="card" style={{border: '1px solid rgba(0,0,0,.125)', padding: '6px', margin: '6px', backgroundColor: '#add8e6'}}>
+      <div className="card" style={{border: '1px solid rgba(0,0,0,.125)', padding: '6px', margin: '6px', backgroundColor: '#eee'}}>
         <div className="card-body">
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">{(this.props.idx + 1).toString() + ". " + this.props.entry.island_id}</h5>
-              <div className="card-info">
-                {this.props.entry.reservationDate}
-              </div>
-              <div className="card-info-dot">Start date: {this.props.entry.startDate} sq.m</div>
+              <h5 className="card-title">{(this.props.idx + 1).toString() + ": " + this.props.entry.island_id}</h5>
+              <div className="card-info-dot">Start Date: {moment(this.props.entry.startDate).format('MM/DD/YYYY')}</div>
+              <div className="card-info-dot">End Date: {moment(this.props.entry.endDate).format('MM/DD/YYYY')}</div>
+              <div className="card-info-dot">Date Reserved: {moment(this.props.entry.reservationDate).format('MM/DD/YYYY')}</div>
               <div className="card-info-dot">
                 Amount Paid: ${this.numberWithCommas(this.props.entry.amountPaid.toFixed(2))}
               </div>
